46b62b13f36f8e77fbeb4c4bb89b406fc2b1190c
tanuj-chamil <tanujchamil@gmail.com>
2022-10-19 16:38:51 +0530

    find peaks from csv

---
+++ csv_read.py
@@ -0,0 +1,12 @@
+import csv
+from matplotlib import pyplot as plt
+
+def get_data(filename):
+    with open(filename) as f:
+            data = csv.reader(f)
+            x = []
+            y = []
+            for row in data:
+                x.append(float(row[0]))
+                y.append(float(row[1]))
+    return x,y
---
+++ find_peaks.py
@@ -0,0 +1,14 @@
+import csv
+from scipy.signal import find_peaks
+
+def detect_peaks(x,y):
+    peaks = find_peaks(y,prominence=1)
+
+    x_peaks = [x[i] for i in peaks[0]]
+    y_peaks = [y[i] for i in peaks[0]]
+
+    return(x_peaks,y_peaks)
+    # print(_x)
+    # plt.plot(x, y)
+    # plt.plot(_x, _y, "xr")
+    # plt.show()
