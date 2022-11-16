47bcea20fcab021b0acebc759c921bb937b20c37
tanuj-chamil <tanujchamil@gmail.com>
2022-11-16 07:41:59 +0530

    changed folder structure into package

--- databases/csv/0.py
+++
@@ -1,9 +0,0 @@
-from os import listdir
-from os.path import isfile, join
-mypath= 'E:\Academic\Semester 05\Charaterization\codebase\project-ramen\databases\csv'
-files = [f for f in listdir(mypath) if isfile(join(mypath, f))][1:]
-
-f =  open('filenames.txt','w')
-f.write(",".join(files))
-f.close()
-
